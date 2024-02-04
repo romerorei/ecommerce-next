17c40ecf212964e8a74c93b3315a62a274685403
Reinaldo Romero <reinaldo.romero@pjchile.com>
2024-02-04 17:50:45 -0300

    Add CRUD address

---
+++ src/actions/address/delete-user-address.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { Address } from "@/interfaces";
+import prisma from "@/lib/prisma";
+
+
+export const deleteUserAddress = async(userId: string) => {
+
+
+  try {
+
+    const deleteAddress = await prisma.userAddress.delete({
+      where: { userId },
+    });
+    console.log({deleteAddress})
+
+    return {
+      ok: true,
+      deleteAddress: deleteAddress,
+    }
+
+
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'No se pudo eliminar la direccion'
+    }
+  }
+}
+
--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,12 +1,13 @@
-import { create } from 'zustand';
-import { initialData } from './seed';
 import prisma from '../lib/prisma';
+import { initialData } from './seed';
+import { countries } from './seed-countries';
 
 
 async function main() {
 
   // 1. Borrar registros previos
   // await Promise.all( [
+  await prisma.userAddress.deleteMany();
   await prisma.user.deleteMany();
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
@@ -17,6 +18,10 @@ async function main() {
 
   await prisma.user.createMany({
     data: users
+  })
+
+  await prisma.country.createMany({
+    data: countries
   })
 
 
